a5503f32930a2a8604ee110128e1145af8079177
Nagapavankumar-23 <70704680+Nagapavankumar-23@users.noreply.github.com>
2020-09-09 20:34:50 +0530

    Update telegram_bot_adafruit.py

--- telegram_bot_adafruit.py
+++ telegram_bot_adafruit.py
@@ -16,21 +16,10 @@ def send_value(value):
   feed = aio.feeds('bot')
   aio.send_data(feed.key,value)
 
-def input_message(update, context):
-  text=update.message.text
-  if text == 'turn on':
-    send_value(1)
-    context.bot.send_message(chat_id=update.effective_chat.id,text="Led turned on")
-    context.bot.send_photo(chat_id=update.effective_chat.id,photo='https://img.icons8.com/plasticine/2x/light-on.png')
-  elif text == 'turn off':
-    send_value(0)
-    context.bot.send_message(chat_id=update.effective_chat.id,text="Led turned off")
-    context.bot.send_photo(chat_id=update.effective_chat.id,photo='https://pngimg.com/uploads/bulb/bulb_PNG1241.png')
-
 def start(update,context):
   start_message='''
-/turnoff or 'turn off':To turn of the led ,sends value=0 in feed
-/turnon or 'turn on'  :To turn on the led ,sends value=1 in feed
+/turnoff :To turn of the light
+/turnon :To turn on the light
 '''
   context.bot.send_message(chat_id=update.effective_chat.id, text=start_message)
 
