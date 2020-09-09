10a5bd8a6af991c623fd9c5b83e4c3d60c796b4d
Nagapavankumar-23 <70704680+Nagapavankumar-23@users.noreply.github.com>
2020-09-10 02:13:23 +0530

    Update telegram_bot_adafruit.py

--- telegram_bot_adafruit.py
+++ telegram_bot_adafruit.py
@@ -9,11 +9,11 @@ TOKEN = os.getenv('TOKEN')
 aio = Client(ADAFRUIT_IO_USERNAME,ADAFRUIT_IO_KEY)
 
 def turnoff(update, context):
-  context.bot.send_message(chat_id=update.effective_chat.id, text="Led turned off")
-  context.bot.send_photo(chat_id=update.effective_chat.id,photo='https://pngimg.com/uploads/bulb/bulb_PNG1241.png')
+  context.bot.send_message(chat_id=update.effective_chat.id, text="Light turned off")
+  context.bot.send_photo(chat_id=update.effective_chat.id,photo='https://www.123rf.com/photo_12676342_filament-lamp-on-a-white-background-illustration-for-design.html')
   send_value(0)
 def turnon(update, context):
-  context.bot.send_message(chat_id=update.effective_chat.id, text="Led turned on")
+  context.bot.send_message(chat_id=update.effective_chat.id, text="Light turned on")
   context.bot.send_photo(chat_id=update.effective_chat.id,photo='https://www.123rf.com/photo_23474570_vector-light-bulb-isolated-on-white.html')
   send_value(1)
 
@@ -25,12 +25,12 @@ def input_message(update, context):
   text=update.message.text
   if text == 'turn on':
     send_value(1)
-    context.bot.send_message(chat_id=update.effective_chat.id,text="Led turned on")
+    context.bot.send_message(chat_id=update.effective_chat.id,text="Light turned on")
     context.bot.send_photo(chat_id=update.effective_chat.id,photo='https://www.123rf.com/photo_23474570_vector-light-bulb-isolated-on-white.html')
   elif text == 'turn off':
     send_value(0)
-    context.bot.send_message(chat_id=update.effective_chat.id,text="Led turned off")
-    context.bot.send_photo(chat_id=update.effective_chat.id,photo='https://pngimg.com/uploads/bulb/bulb_PNG1241.png')
+    context.bot.send_message(chat_id=update.effective_chat.id,text="Light turned off")
+    context.bot.send_photo(chat_id=update.effective_chat.id,photo='https://www.123rf.com/photo_12676342_filament-lamp-on-a-white-background-illustration-for-design.html')
 
 def start(update,context):
   start_message='''
