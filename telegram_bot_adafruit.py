3bcfa7bc3017749fe8e0b21cb66ffb5e1b996edd
Nagapavankumar-23 <70704680+Nagapavankumar-23@users.noreply.github.com>
2020-09-10 01:20:27 +0530

    Update telegram_bot_adafruit.py

--- telegram_bot_adafruit.py
+++ telegram_bot_adafruit.py
@@ -8,6 +8,14 @@ ADAFRUIT_IO_KEY = os.getenv('ADAFRUIT_IO_KEY')
 TOKEN = os.getenv('TOKEN')
 
 
+def start(update,context):
+  start_message='''
+/turnoff or 'turn off': To OFF the light
+/turnon or 'turn on': To ON the light
+'''
+  context.bot.send_message(chat_id=update.effective_chat.id, text=start_message)
+
+  
 def turnoff(update, context):
   context.bot.send_message(chat_id=update.effective_chat.id, text="Led turned off")
   context.bot.send_photo(chat_id=update.effective_chat.id,photo='https://pngimg.com/uploads/bulb/bulb_PNG1241.png')
@@ -32,12 +40,6 @@ def input_message(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id,text="Led turned off")
     context.bot.send_photo(chat_id=update.effective_chat.id,photo='https://pngimg.com/uploads/bulb/bulb_PNG1241.png')
 
-def start(update,context):
-  start_message='''
-/turnoff or 'turn off': To OFF the light
-/turnon or 'turn on': To ON the light
-'''
-  context.bot.send_message(chat_id=update.effective_chat.id, text=start_message)
 
 
 aio = Client(ADAFRUIT_IO_USERNAME,ADAFRUIT_IO_KEY)
